cd031c33871ee1505cc60ad4d1c33ad80045da9e
jazzysnake <dotya77@gmail.com>
2022-05-17 00:04:17 +0200

    custom network config

---
+++ start-network.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+export MICROFAB_CONFIG='{
+    "endorsing_organizations":[
+        {
+            "name": "RailwayOrg"
+        },
+        {
+            "name": "VehicleOwnerOrg"
+        }
+    ],
+    "channels":[
+        {
+            "name": "railway-channel",
+            "endorsing_organizations":[
+                "RailwayOrg",
+                "VehicleOwnerOrg"
+            ]
+        }
+    ]
+}'
+
+docker run -p 8080:8080 -e MICROFAB_CONFIG ibmcom/ibp-microfab
